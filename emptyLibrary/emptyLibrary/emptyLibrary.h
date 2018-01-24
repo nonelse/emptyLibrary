89b2afcec86c72e1d6e7e852584d0ce7c6ee4a06
nonelse <pedro@adjust.com>
2018-01-24 11:06:21 +0100

    Add empty library

---
+++ emptyLibrary/emptyLibrary/emptyLibrary.h
@@ -0,0 +1,19 @@
+//
+//  emptyLibrary.h
+//  emptyLibrary
+//
+//  Created by Pedro on 24.01.18.
+//  Copyright © 2018 adjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for emptyLibrary.
+FOUNDATION_EXPORT double emptyLibraryVersionNumber;
+
+//! Project version string for emptyLibrary.
+FOUNDATION_EXPORT const unsigned char emptyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <emptyLibrary/PublicHeader.h>
+
+
